cfa9641df9ce9d297d036f428fa706953f26be76
Camila Guilherme <mila.valentine@gmail.com>
2020-11-14 15:22:08 -0300

    URL change to connect with gateway

--- URLs.js
+++ URLs.js
@@ -1,4 +1,4 @@
 module.exports = {
-    SALES_API_URL: 'http://localhost:3001',
-    PRODUCTS_API_URL: 'http://localhost:3002'
+    SALES_API_URL: 'api-sales:3001',
+    PRODUCTS_API_URL: 'api-products:3002'
 }
