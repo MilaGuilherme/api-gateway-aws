cc738111b95d5a1df7dfe10211940bef6b76647c
mathesbh <mathesbh@gmail.com>
2020-11-14 12:17:26 -0300

    Configuring routes (GetId, Post, Put, Delete)

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const productsServiceProxy = httpProxy(PRODUCTS_API_URL)
 app.get('/', (req, res) => res.send('Hello Gateway API'))
 
 app.get('/sales', (req, res, next) => salesServiceProxy(req, res, next))
+app.get('/sales/:id', (req, res, next) => salesServiceProxy(req, res, next))
+app.post('/sales', (req, res, next) => salesServiceProxy(req, res, next))
+app.delete('/sales/:id', (req, res, next) => salesServiceProxy(req, res, next))
 app.get('/products', (req, res, next) => productsServiceProxy(req, res, next))
+app.post('/products', (req, res, next) => productsServiceProxy(req, res, next))
+app.put('/products/:id', (req, res, next) => productsServiceProxy(req, res, next))
+app.delete('/products/:id', (req, res, next) => productsServiceProxy(req, res, next))
 
 app.listen(PORT, () =>{
     console.log(`API gateway listening on port ${PORT}`)
